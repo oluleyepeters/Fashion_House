36455162c66a2566f1c03a0fe9f5e5895e4ac51d
oluleyepeters <oluleyepeters@gmail.com>
2019-12-30 17:02:44 +0100

    checkbox implementation

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
 module.exports={
 	mongoURI:process.env.MONGO_URI,
 	domain: process.env.REDIRECT_DOMAIN,
-	public_vapid_Key: process.env.Vapid_Private,
-	private_vapid_Key: process.env.Vapid_Public,	
+	public_vapid_Key: process.env.Vapid_Public,
+	private_vapid_Key: process.env.Vapid_Private,	
 };
 
 //mongo ds257668.mlab.com:57668/fashion_house -u <dbuser> -p <dbpassword>
