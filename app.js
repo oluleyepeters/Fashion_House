bd7b61581b2c4001954943c8a554e6e2d0fa889c
oluleyepeters <oluleyepeters@gmail.com>
2020-01-18 02:14:19 +0100

    alert

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ webpush.setVapidDetails(
 	'mailto: oluleyepeters@gmail.com',
 	publicKey,
 	privateKey
-)
+) 
 
 mongoose.promise = global.Promise;
 
@@ -62,9 +62,8 @@ app.use(flash())
 
 //Global Variables
 app.use(function(req,res,next){
-	res.locals.success_msg = req.flash('success_msg');
-	res.locals.error_msg = req.flash('error_msg');
-	res.locals.error = req.flash('error');
+	res.locals.success = req.flash('success_msg');
+	res.locals.error = req.flash('error_msg');
 	res.locals.user = req.user || null;
 	next();
 })
